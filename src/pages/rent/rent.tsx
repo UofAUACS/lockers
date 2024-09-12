da069cc7e965831de783fd7e1b5e751126e1b0d2
mansooranis <mansooranis60@gmail.com>
2024-09-11 23:59:54 -0400

    Fixed Token display

--- src/pages/rent/rent.tsx
+++ src/pages/rent/rent.tsx
@@ -42,7 +42,6 @@ export default function Rent() {
                 }
             })
             setAvailableLockers(res.data.lockers)
-            console.log(token)
         }
         
         getData()
